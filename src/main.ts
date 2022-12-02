06e080c22a4e50edc6f1d75acdd8dc9107a83255
Julien Champoux <2070270@cegepmontpetit.ca>
2022-12-02 16:09:14 -0500

    Simple settings GUI

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import './style.css';
 import {TreeVisualizationApp} from "./app";
 import {BlinkingStarsAnimator} from "./effects/blinking_stars";
+import GUI from "lil-gui";
 
-const LIGHT_COUNT = 400;
 const TREE_RADIUS = 1.8;
 const TREE_HEIGHT = 4;
 
-const app = new TreeVisualizationApp(TREE_HEIGHT, TREE_RADIUS, LIGHT_COUNT);
+const settings = {
+    lightCount: 400
+};
+
+const app = new TreeVisualizationApp(TREE_HEIGHT, TREE_RADIUS, settings.lightCount);
 app.setAnimator(new BlinkingStarsAnimator());
 
 function doFrame() {
@@ -15,3 +19,8 @@ function doFrame() {
     app.render();
 }
 doFrame();
+
+const gui = new GUI();
+gui.add(settings, 'lightCount', 10, 1000, 1)
+    .name('Light Count')
+    .onChange((value: number) => app.setupLights(value));
